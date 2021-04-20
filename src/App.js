7010efaab9daeb6eebc881928598b7655267a05b
Ieieieie <live_house_0608@ezweb.ne.jp>
2021-04-20 17:38:24 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import { List } from "./List";
 import { useState } from 'react';
+import React from 'react';
+// function App() {
+//   const [description, setDescription] = useState('クリック前の表示');
+// const changeDescription = () => {
+//   setDescription('クリック後の表示')
+// }
 
-function App() {
-  const [description, setDescription] = useState('クリック前の表示');
-const changeDescription = () => {
-  setDescription('クリック後の表示')
-}
+//   return (
+//     <div>ゼロから始めるReact入門
+//       { description }
+//       <List title = "取扱言語一覧" />
+//       <button onClick={ changeDescription }>button</button>
+//     </div>
+//   );
+// }
 
-  return (
-    <div>ゼロから始めるReact入門
-      { description }
-      <List title = "取扱言語一覧" />
-      <button onClick={ changeDescription }>button</button>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {description: 'クリック前の表示'}
+  }
+  changeDescription(){
+    this.setState({
+      description: 'クリック後の表示です'
+    })
+  }
+  render(){
+    const { description } = this.state;
+    return(
+      <div>クラスコンポーネントにしてみました<br/>
+        {description}
+        <List title="取扱言語一覧" />
+        <button onClick ={() => this.changeDescription()}>button</button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,13 +1,27 @@
-export const List = (props) => {
-    const title = props.title;
-    return(
-        <div>
-            <h4>{ title }</h4>
-            <div>
-                List
-            </div>
+import React from 'react';
+// export const List = (props) => {
+//     const title = props.title;
+//     return(
+//         <div>
+//             <h4>{ title }</h4>
+//             <div>
+//                 List
+//             </div>
 
-        </div>
+//         </div>
         
-    )
+//     )
+// }
+
+export class List extends React.Component{
+    render(){
+        const { title } = this.props;
+        return(
+            <div>
+                <h4>{title}</h4>
+                <div>リストです</div>
+            </div>
+        )
+    }
+    
 }
